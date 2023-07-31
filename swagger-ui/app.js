9d1feb300d50207c78b4297a5cc800d32ab8fe35
Adam <adam.kowalczuk@poczta.fm>
2023-07-31 18:38:46 +0200

    First commit

---
+++ swagger-ui/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const swaggerUI = require("swagger-ui-express");
+
+const app = express();
+app.use(
+  "/swagger",
+  swaggerUI.serve,
+  swaggerUI.setup(null, {
+    swaggerOptions: {
+      url: process.env.SWAGGER_SPEC_URL,
+    },
+  })
+);
+
+module.exports.handler = serverless(app);
