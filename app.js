d66f5317afe279d0255ff1b59c19aa7891f861f7
Adam <adam.kowalczuk@poczta.fm>
2023-07-31 20:39:51 +0200

    Change swagger options

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const swaggerUI = require("swagger-ui-express");
-
+const swaggerDocument = YAML.load("./swagger.yaml"); // Load the Swagger YAML file
 const app = express();
-app.use(
-  "/swagger",
-  swaggerUI.serve,
-  swaggerUI.setup(null, {
-    swaggerOptions: {
-      url: process.env.SWAGGER_SPEC_URL,
-    },
-  })
-);
+app.use("/swagger", swaggerUI.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
